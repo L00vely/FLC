2131a6eec6132f3d2e545a142356f0c6fbc9d0e3
L00vely <peralesrodriguezdavid@gmail.com>
2023-07-20 17:18:18 -0600

    fix: Quitando circulares extras

--- src/util/circulares.js
+++ src/util/circulares.js
@@ -11,23 +11,23 @@ export const circulares = [
         date: '11 de Julio del 2023',
         title: 'CIRCULAR 31/23'
     },
-    //Prueba
-    {
-        pdf: '/circulares/31-23.pdf',
-        img: '/circulares/31-23.png',
-        date: '11 de Julio del 2023',
-        title: 'CIRCULAR 30/23'
-    },
-    {
-        pdf: '/circulares/31-23.pdf',
-        img: '/circulares/31-23.png',
-        date: '11 de Julio del 2023',
-        title: 'CIRCULAR 29/23'
-    },
-    {
-        pdf: '/circulares/31-23.pdf',
-        img: '/circulares/31-23.png',
-        date: '11 de Julio del 2023',
-        title: 'CIRCULAR 28/23'
-    },
+    // //Prueba
+    // {
+    //     pdf: '/circulares/31-23.pdf',
+    //     img: '/circulares/31-23.png',
+    //     date: '11 de Julio del 2023',
+    //     title: 'CIRCULAR 30/23'
+    // },
+    // {
+    //     pdf: '/circulares/31-23.pdf',
+    //     img: '/circulares/31-23.png',
+    //     date: '11 de Julio del 2023',
+    //     title: 'CIRCULAR 29/23'
+    // },
+    // {
+    //     pdf: '/circulares/31-23.pdf',
+    //     img: '/circulares/31-23.png',
+    //     date: '11 de Julio del 2023',
+    //     title: 'CIRCULAR 28/23'
+    // },
 ]
