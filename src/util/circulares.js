e5576caac7e0a5b94158123af6f750eee1cf58f0
L00vely <peralesrodriguezdavid@gmail.com>
2023-08-11 09:51:51 -0600

    circular: 7 de agosto

--- src/util/circulares.js
+++ src/util/circulares.js
@@ -1,22 +1,29 @@
 export const circulares = [
     {
+        pdf: '/circulares/34-23.pdf',
+        img: '/circulares/34-23.png',
+        date: '07 de agosto del 2023',
+        title: 'CIRCULAR 34/23',
+        link: ''
+    },
+    {
         pdf: '/circulares/33-23.pdf',
         img: '/circulares/33-23.png',
-        date: '24 de Julio del 2023',
+        date: '24 de julio del 2023',
         title: 'CIRCULAR 33/23',
         link: 'https://www.linkedin.com/posts/lic-jos%C3%A9-carlos-gonzalez-araiza-3024871a5_suplantacion-de-identidad-activity-7089301119960539137-hhkR?utm_source=share&utm_medium=member_desktop'
     }
     ,{
         pdf: '/circulares/32-23.pdf',
         img: '/circulares/32-23.png',
-        date: '19 de Julio del 2023',
+        date: '19 de julio del 2023',
         title: 'CIRCULAR 32/23',
         link: ''
     },
     {
         pdf: '/circulares/31-23.pdf',
         img: '/circulares/31-23.png',
-        date: '11 de Julio del 2023',
+        date: '11 de julio del 2023',
         title: 'CIRCULAR 31/23',
         link: 'https://www.linkedin.com/posts/lic-jos%C3%A9-carlos-gonzalez-araiza-3024871a5_termina-prorroga-para-cumplir-requisitos-activity-7084564199695081472-Q_Um?utm_source=share&utm_medium=member_desktop'
     },
