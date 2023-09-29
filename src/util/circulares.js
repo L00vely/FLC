c5492f6ba02e95733ee8076d70000ff09ce5b424
DeiPR <david.p@gluo.mx>
2023-09-29 14:06:24 -0600

    feat: 43-23

--- src/util/circulares.js
+++ src/util/circulares.js
@@ -1,10 +1,17 @@
 export const circulares = [
     {
+        pdf: '/circulares/42-23.pdf',
+        img: '/circulares/42-23.png',
+        date: '29 de septiembre del 2023',
+        title: 'CIRCULAR 42/23',
+        link: ''  
+    },
+    {
         pdf: '/circulares/41-23.pdf',
         img: '/circulares/41-23.png',
         date: '20 de septiembre del 2023',
         title: 'CIRCULAR 41/23',
-        link: ''
+        link: 'https://www.linkedin.com/posts/fiscallegumcorporatum_las-obligaciones-patronales-por-horas-extra-activity-7112139660771905536-rg7h?utm_source=share&utm_medium=member_desktop'
     },
     {
         pdf: '/circulares/40-23.pdf',
@@ -68,24 +75,5 @@ export const circulares = [
         date: '11 de julio del 2023',
         title: 'CIRCULAR 31/23',
         link: 'https://www.linkedin.com/posts/lic-jos%C3%A9-carlos-gonzalez-araiza-3024871a5_termina-prorroga-para-cumplir-requisitos-activity-7084564199695081472-Q_Um?utm_source=share&utm_medium=member_desktop'
-    },
-    // //Prueba
-    // {
-    //     pdf: '/circulares/31-23.pdf',
-    //     img: '/circulares/31-23.png',
-    //     date: '11 de Julio del 2023',
-    //     title: 'CIRCULAR 30/23'
-    // },
-    // {
-    //     pdf: '/circulares/31-23.pdf',
-    //     img: '/circulares/31-23.png',
-    //     date: '11 de Julio del 2023',
-    //     title: 'CIRCULAR 29/23'
-    // },
-    // {
-    //     pdf: '/circulares/31-23.pdf',
-    //     img: '/circulares/31-23.png',
-    //     date: '11 de Julio del 2023',
-    //     title: 'CIRCULAR 28/23'
-    // },
+    }
 ]
