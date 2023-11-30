3caaedf472bdff87a783ce5ec4ff86b2d22ba155
DeiPR <david.p@gluo.mx>
2023-11-30 01:18:34 -0600

    fix: Whatsapp button

--- src/components/ui/WhatsAppButton/WhatsAppButton.jsx
+++ src/components/ui/WhatsAppButton/WhatsAppButton.jsx
@@ -1,32 +1,36 @@
-import { Button, Link } from "@chakra-ui/react";
-import { FaWhatsapp } from 'react-icons/fa';
+import { PhoneIcon } from "@chakra-ui/icons";
+import { Button, Text } from "@chakra-ui/react";
 
 export const WhatsAppButton = ( { service } ) => {
 
     const phoneNumbers = ['+525587648456', '+525580132125', '+525573547093', '+525514178078'];
-        // const phoneNumbers = ['+525587648456'];
-
-
+      
     const defaultMessage = `Hola, me gustaría saber más sobre el servicio de ${service} a través del sitio web de Fiscal Legum Corporatum.`;
 
-    const whatsappUrls = phoneNumbers.map((phoneNumber) => {
-        return `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(defaultMessage)}`;
+    const handleButtonClick = () => {
+      phoneNumbers.forEach((phoneNumber) => {
+        const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(defaultMessage)}`;
+        window.open(whatsappUrl, '_blank');
       });
-    
-      const handleButtonClick = () => {
-        whatsappUrls.forEach((url) => {
-          window.open(url, '_blank');
-        });
-      };
-
+    };
 
     return (
         <Button 
             colorScheme="green"
-            leftIcon={<FaWhatsapp />}
             onClick={handleButtonClick}
-        >
+            p="2rem"
+            display="flex"
+            flexDirection="row"
+            gap="1rem"
+        > 
+          <PhoneIcon />
+          
+          <Text as="span">
             Contacta a nuestro equipo de forma directa
+
+
+          </Text>
+
         </Button>
     );
 };
